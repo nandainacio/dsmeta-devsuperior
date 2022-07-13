cc77930319cb0cf331bd4a4f30d771ab87183869
nandainacio <fp_inacio@hotmail.com>
2022-07-13 20:18:39 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from '../../assets/img/logo.svg'
+import './style.css'
+
+function Header(){
+
+    return (
+
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="Logo DSMeta"/>
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por <a href="https://github.com/nandainacio" target="_blank"></a> @nandainacio</p>
+            </div>
+        </header>
+
+    )
+
+
+}
+
+export default Header
